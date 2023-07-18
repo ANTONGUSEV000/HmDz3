d3e257112cfdfdab1b3eaa1f6f77c1413df584ca
Anton Gusev <antongusev705@gmail.com>
2023-07-18 13:20:55 +0300

    Задача 34

---
+++ D1/Program.cs
@@ -0,0 +1,29 @@
+﻿  Random random = new Random();
+
+        // Задаем размер массива
+        int size = 100;
+
+        // Создаем массив
+        int[] numbers = new int[size];
+
+        // Заполняем массив случайными положительными трехзначными числами
+        for (int i = 0; i < size; i++)
+        {
+            numbers[i] = random.Next(100, 1000);
+        }
+
+        // Переменная для подсчета количества четных чисел
+        int evenCount = 0;
+
+        // Перебираем все элементы массива и проверяем их на четность
+        foreach (int number in numbers)
+        {
+            if (number % 2 == 0)
+            {
+                evenCount++;
+            }
+        }
+
+        // Выводим результат
+        Console.WriteLine("Количество четных чисел: " + evenCount);
+    
